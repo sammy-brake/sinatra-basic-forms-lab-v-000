9c09988150dc7828c6b50bbeb5a932a072e9d850
Samantha brake <sammy.brake@gmail.com>
2018-12-05 01:15:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/puppy' do
+    @name = params[:name]
+    @age = params[:age]
+    @breed = params[:breed]
+    erb :puppy
+  end
 end
